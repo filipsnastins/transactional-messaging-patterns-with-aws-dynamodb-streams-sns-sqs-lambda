37000465c6b2142b605c0c9cc2593de747851445
Filips Nastins <nastinsfilips@gmail.com>
2023-07-27 08:00:58 +0200

    improve event structure

--- service-customers/src/customers/events.py
+++ service-customers/src/customers/events.py
@@ -1,17 +1,18 @@
+import datetime
 import uuid
-from dataclasses import dataclass
-from datetime import datetime
+from dataclasses import dataclass, field
 from decimal import Decimal
 
 
 @dataclass(kw_only=True)
 class Event:
-    event_id: uuid.UUID
-    created_at: datetime
+    event_id: uuid.UUID = field(default_factory=uuid.uuid4)
+    created_at: datetime.datetime
 
 
 @dataclass(kw_only=True)
 class CustomerCreatedEvent(Event):
-    id: uuid.UUID
+    customer_id: uuid.UUID
     name: str
     credit_limit: Decimal
+    created_at: datetime.datetime
