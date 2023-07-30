e7e4419e3fd62c9c928617c2d08ded7ef3ea6db4
Filips Nastins <nastinsfilips@gmail.com>
2023-07-30 14:11:10 +0200

    lint tf and deploy service-orders to AWS ECS

--- deploy/aws-ecs-terraform/ecr.tf
+++ deploy/aws-ecs-terraform/ecr.tf
@@ -1,3 +1,7 @@
 resource "aws_ecr_repository" "service_customers" {
-  name = "tomodachi-transactional-outbox-service-customers"
+  name = "${var.environment}-tomodachi-transactional-outbox-service-customers"
+}
+
+resource "aws_ecr_repository" "service_orders" {
+  name = "${var.environment}-tomodachi-transactional-outbox-service-orders"
 }
--- deploy/aws-ecs-terraform/main.tf
+++ deploy/aws-ecs-terraform/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.4.4"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
