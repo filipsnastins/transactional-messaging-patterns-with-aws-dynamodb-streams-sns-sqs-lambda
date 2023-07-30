a25291005917f1885f795d66b73310aa8cbd9259
Filips Nastins <nastinsfilips@gmail.com>
2023-07-30 14:10:21 +0200

    service-orders template

---
+++ service-orders/src/entrypoints/tomodachi_app.py
@@ -0,0 +1,10 @@
+import tomodachi
+from aiohttp import web
+
+
+class TomodachiService(tomodachi.Service):
+    name = "service-orders"
+
+    @tomodachi.http("GET", r"/health/?", ignore_logging=[200])
+    async def healthcheck(self, request: web.Request) -> web.Response:
+        return web.json_response({"status": "ok"}, status=200)
