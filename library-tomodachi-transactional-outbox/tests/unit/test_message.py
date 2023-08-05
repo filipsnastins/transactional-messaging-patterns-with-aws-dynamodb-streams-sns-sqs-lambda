4f090d37cb264913da9bda857fa6ff1c669b41dd
Filips Nastins <nastinsfilips@gmail.com>
2023-08-05 10:22:34 +0300

    WIP: update message models

--- library-tomodachi-transactional-outbox/tests/unit/test_message.py
+++ library-tomodachi-transactional-outbox/tests/unit/test_message.py
@@ -2,7 +2,7 @@ import datetime
 import json
 import uuid
 
-from tomodachi_transactional_outbox.message import Message
+from tomodachi_transactional_outbox.message import DispatchedMessage, Message
 
 
 def test_message_model() -> None:
@@ -26,5 +26,29 @@ def test_message_model() -> None:
     assert message.topic == "test-topic"
     assert message.message == json.dumps({"message": "test-message"})
     assert message.created_at == created_at
-    assert message.dispatched is False
-    assert message.dispatched_at is None
+
+
+def test_dispatched_message_model() -> None:
+    message_id = uuid.uuid4()
+    aggregate_id = uuid.uuid4()
+    correlation_id = uuid.uuid4()
+    created_at = datetime.datetime.utcnow().replace(tzinfo=datetime.UTC)
+    dispatched_at = datetime.datetime.utcnow().replace(tzinfo=datetime.UTC)
+
+    message = DispatchedMessage(
+        message_id=message_id,
+        aggregate_id=aggregate_id,
+        correlation_id=correlation_id,
+        topic="test-topic",
+        message=json.dumps({"message": "test-message"}),
+        created_at=created_at,
+        dispatched_at=dispatched_at,
+    )
+
+    assert message.message_id == message_id
+    assert message.aggregate_id == aggregate_id
+    assert message.correlation_id == correlation_id
+    assert message.topic == "test-topic"
+    assert message.message == json.dumps({"message": "test-message"})
+    assert message.created_at == created_at
+    assert message.dispatched_at == dispatched_at
