e86ae6fbebf7291d3cd03e0c30a187e32be05734
Filips Nastins <nastinsfilips@gmail.com>
2023-08-07 13:26:22 +0200

    chore: split use case tests into multiple files

---
+++ service-customers/tests/fakes.py
@@ -0,0 +1,56 @@
+import copy
+import uuid
+
+from tomodachi_outbox.message import Message
+
+from adapters.customer_repository import AbstractCustomerRepository
+from adapters.event_repository import AbstractEventRepository
+from customers.customer import Customer, CustomerNotFoundError
+from customers.events import Event
+from service_layer.unit_of_work import AbstractUnitOfWork
+
+
+class FakeCustomerRepository(AbstractCustomerRepository):
+    def __init__(self, customers: list[Customer]) -> None:
+        super().__init__()
+        self._customers = customers
+
+    async def create(self, customer: Customer) -> None:
+        self._customers.append(copy.deepcopy(customer))
+
+    async def save(self, customer: Customer) -> None:
+        indices = [i for i, x in enumerate(self._customers) if x.id == customer.id]
+        if not len(indices):
+            raise CustomerNotFoundError(customer.id)
+        assert len(indices) == 1
+        self._customers[indices[0]] = copy.deepcopy(customer)
+
+    async def get(self, customer_id: uuid.UUID) -> Customer | None:
+        return next((copy.deepcopy(v) for v in self._customers if v.id == customer_id), None)
+
+
+class FakeEventRepository(AbstractEventRepository):
+    def __init__(self, events: list[Event]) -> None:
+        self.events = events
+
+    async def publish(self, events: list[Event]) -> None:
+        self.events.extend(copy.deepcopy(events))
+
+    async def get(self, event_id: uuid.UUID) -> Message | None:
+        pass
+
+
+class FakeUnitOfWork(AbstractUnitOfWork):
+    customers: FakeCustomerRepository
+    events: FakeEventRepository
+
+    def __init__(self) -> None:
+        self.customers = FakeCustomerRepository([])
+        self.events = FakeEventRepository([])
+        self.committed = False
+
+    async def commit(self) -> None:
+        self.committed = True
+
+    async def rollback(self) -> None:
+        pass
---
+++ service-customers/tests/unit/test_create_customer.py
@@ -0,0 +1,52 @@
+import uuid
+from decimal import Decimal
+
+import pytest
+
+from customers.commands import CreateCustomerCommand
+from customers.events import CustomerCreatedEvent
+from service_layer import use_cases
+from tests.fakes import FakeUnitOfWork
+
+
+@pytest.mark.asyncio()
+async def test_create_customer() -> None:
+    uow = FakeUnitOfWork()
+    cmd = CreateCustomerCommand(name="John Doe", credit_limit=Decimal("200.00"))
+
+    customer = await use_cases.create_customer(uow, cmd)
+
+    assert uow.committed is True
+    assert customer == (await uow.customers.get(customer.id))
+    assert isinstance(customer.id, uuid.UUID)
+    assert customer.name == "John Doe"
+    assert customer.credit_limit == Decimal("200.00")
+    assert customer.credit_reservations == {}
+    assert customer.version == 0
+    assert customer.created_at
+
+
+@pytest.mark.asyncio()
+async def test_customer_created_event_published() -> None:
+    uow = FakeUnitOfWork()
+    cmd = CreateCustomerCommand(name="John Doe", credit_limit=Decimal("200.00"))
+
+    customer = await use_cases.create_customer(uow, cmd)
+    [event] = uow.events.events
+
+    assert isinstance(event, CustomerCreatedEvent)
+    assert isinstance(event.event_id, uuid.UUID)
+    assert event.event_id != customer.id
+    assert event.customer_id == customer.id
+    assert event.name == "John Doe"
+    assert event.credit_limit == Decimal("200.00")
+    assert event.created_at == customer.created_at
+
+
+@pytest.mark.asyncio()
+async def test_new_customer_has_full_available_credit() -> None:
+    uow = FakeUnitOfWork()
+    cmd = CreateCustomerCommand(name="John Doe", credit_limit=Decimal("200.00"))
+    customer = await use_cases.create_customer(uow, cmd)
+
+    assert customer.available_credit() == Decimal("200.00")
