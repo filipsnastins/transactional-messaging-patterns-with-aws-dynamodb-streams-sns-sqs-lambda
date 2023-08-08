5e4a61dd6238609a4a8d546f60cdab2a18bb35e7
Filips Nastins <nastinsfilips@gmail.com>
2023-08-08 11:16:17 +0200

    refactor: move repository errors to repo module

--- service-customers/tests/integration/test_customer_repository.py
+++ service-customers/tests/integration/test_customer_repository.py
@@ -3,7 +3,8 @@ from decimal import Decimal
 
 import pytest
 
-from customers.customer import Customer, CustomerNotFoundError, OptimisticLockError
+from adapters.customer_repository import CustomerNotFoundError, OptimisticLockError
+from customers.customer import Customer
 from service_layer.unit_of_work import DynamoDBUnitOfWork
 
 pytestmark = pytest.mark.usefixtures("_mock_dynamodb")
