3f78f619804dc726f2a3d0ec82d8a8cfd962e343
Filips Nastins <nastinsfilips@gmail.com>
2023-08-09 18:13:25 +0200

    test: order already exists in DynamoDB repository

--- service-orders/tests/integration/test_order_repository.py
+++ service-orders/tests/integration/test_order_repository.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 
 import pytest
 
+from adapters.order_repository import OrderAlreadyExistsError
 from orders.order import Order
 from service_layer.unit_of_work import DynamoDBUnitOfWork
 
@@ -19,3 +20,15 @@ async def test_create_order() -> None:
 
     order_from_db = await uow.orders.get(order.id)
     assert order_from_db == order
+
+
+@pytest.mark.asyncio()
+async def test_order_already_exists() -> None:
+    uow = DynamoDBUnitOfWork.create()
+    order = Order.create(customer_id=uuid.uuid4(), total_amount=Decimal("123.99"))
+    await uow.orders.create(order)
+    await uow.commit()
+
+    await uow.orders.create(order)
+    with pytest.raises(OrderAlreadyExistsError, match=str(order.id)):
+        await uow.commit()
