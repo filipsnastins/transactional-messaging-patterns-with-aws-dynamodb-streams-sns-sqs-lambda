510b66722ad0972cd097d5dfbb7e92be79a0e1d5
Filips Nastins <nastinsfilips@gmail.com>
2023-08-12 09:27:59 +0200

    refactor: create customer response

--- service-customers/src/service_layer/views.py
+++ service-customers/src/service_layer/views.py
@@ -2,19 +2,17 @@ import uuid
 
 import structlog
 
-from service_layer.response import CustomerNotFoundErrorResponse, GetCustomerResponse
+from service_layer.response import FailureResponse, GetCustomerResponse, ResponseTypes
 from service_layer.unit_of_work import AbstractUnitOfWork
 
 logger: structlog.stdlib.BoundLogger = structlog.get_logger()
 
 
-async def get_customer(
-    uow: AbstractUnitOfWork, customer_id: uuid.UUID
-) -> GetCustomerResponse | CustomerNotFoundErrorResponse:
+async def get_customer(uow: AbstractUnitOfWork, customer_id: uuid.UUID) -> GetCustomerResponse | FailureResponse:
     log = logger.bind(customer_id=customer_id)
     customer = await uow.customers.get(customer_id)
     if not customer:
         log.error("customer_not_found")
-        return CustomerNotFoundErrorResponse.create(customer_id)
+        return FailureResponse.create(ResponseTypes.CUSTOMER_NOT_FOUND_ERROR, customer_id=customer_id)
     log.info("get_customer")
     return GetCustomerResponse.create(customer)
