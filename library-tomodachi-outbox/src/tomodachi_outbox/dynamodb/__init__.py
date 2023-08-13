498465a3e5e03843b266b7f193cbe5b09d0b9060
Filips Nastins <nastinsfilips@gmail.com>
2023-08-13 08:42:30 +0200

    refactor: remove unit of work

--- library-tomodachi-outbox/src/tomodachi_outbox/dynamodb/__init__.py
+++ library-tomodachi-outbox/src/tomodachi_outbox/dynamodb/__init__.py
@@ -2,10 +2,8 @@ from tomodachi_outbox.dynamodb.client import DynamoDBClientFactory
 from tomodachi_outbox.dynamodb.repository import DynamoDBOutboxRepository
 from tomodachi_outbox.dynamodb.session import DynamoDBSession
 from tomodachi_outbox.dynamodb.table import create_outbox_table
-from tomodachi_outbox.dynamodb.unit_of_work import BaseDynamoDBUnitOfWork
 
 __all__ = [
-    "BaseDynamoDBUnitOfWork",
     "DynamoDBClientFactory",
     "DynamoDBOutboxRepository",
     "DynamoDBSession",
