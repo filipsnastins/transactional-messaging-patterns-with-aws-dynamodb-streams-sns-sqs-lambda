d83b0d234b43a9ad454e8e685e3373f882604a57
Filips Nastins <nastinsfilips@gmail.com>
2023-08-13 08:36:56 +0200

    feat: move commit to DynamoDB session

--- library-tomodachi-outbox/src/tomodachi_outbox/dynamodb/session.py
+++ library-tomodachi-outbox/src/tomodachi_outbox/dynamodb/session.py
@@ -1,7 +1,12 @@
 from contextvars import ContextVar
 from typing import TypedDict
 
+import structlog
 from types_aiobotocore_dynamodb.type_defs import TransactWriteItemTypeDef
+
+from tomodachi_outbox.dynamodb.client import DynamoDBClientFactory
+
+logger: structlog.stdlib.BoundLogger = structlog.get_logger()
 
 
 class DynamoDBSessionItems(TypedDict):
@@ -12,8 +17,9 @@ class DynamoDBSessionItems(TypedDict):
 class DynamoDBSession:
     _session: ContextVar[list[DynamoDBSessionItems]]
 
-    def __init__(self) -> None:
-        self._session = ContextVar("__dynamodb_session", default=[])
+    def __init__(self, client_factory: DynamoDBClientFactory) -> None:
+        self._get_client = client_factory
+        self._session = ContextVar("__tomodachi_outbox.dynamodb.session", default=[])
 
     def add(
         self, transact_item: TransactWriteItemTypeDef, raise_on_condition_check_failure: Exception | None = None
@@ -24,8 +30,29 @@ class DynamoDBSession:
         )
         self._session.get().append(item)
 
-    def get(self) -> list[DynamoDBSessionItems]:
-        return self._session.get()
+    async def commit(self) -> None:
+        items = self._session.get()
+        if not items:
+            logger.debug("dynamodb_session__no_items_to_commit")
+            return
+        async with self._get_client() as client:
+            try:
+                transact_items = [item["transact_item"] for item in items]
+                await client.transact_write_items(TransactItems=transact_items)
+                logger.debug("dynamodb_session__committed", item_count=len(items))
+            except client.exceptions.TransactionCanceledException as e:
+                cancellation_codes = [reason["Code"] for reason in e.response["CancellationReasons"]]
+                raise_on_condition_failures = [item["raise_on_condition_check_failure"] for item in items]
+                zipped = zip(cancellation_codes, raise_on_condition_failures)
+                for cancellation_code, raise_on_condition_failure in zipped:
+                    if cancellation_code == "None":
+                        continue
+                    if cancellation_code == "ConditionalCheckFailed" and raise_on_condition_failure is not None:
+                        raise raise_on_condition_failure from e
+                raise
+            finally:
+                self.rollback()
 
-    def clear(self) -> None:
+    def rollback(self) -> None:
         self._session.get().clear()
+        logger.debug("dynamodb_session__rolled_back")
---
+++ library-tomodachi-outbox/tests/dynamodb/test_dynamodb_session.py
@@ -0,0 +1,185 @@
+import pytest
+import pytest_asyncio
+from aiobotocore.session import get_session
+from botocore.exceptions import ClientError
+from tomodachi_testcontainers.containers import MotoContainer
+from types_aiobotocore_dynamodb import DynamoDBClient
+from types_aiobotocore_dynamodb.type_defs import TransactWriteItemTypeDef
+
+from tomodachi_outbox.dynamodb import DynamoDBSession
+
+pytestmark = pytest.mark.usefixtures("_create_table", "_reset_moto_container_on_teardown")
+
+
+@pytest_asyncio.fixture()
+async def _create_table(moto_dynamodb_client: DynamoDBClient) -> None:
+    await moto_dynamodb_client.create_table(
+        TableName="TestTable",
+        AttributeDefinitions=[{"AttributeName": "Id", "AttributeType": "S"}],
+        KeySchema=[{"AttributeName": "Id", "KeyType": "HASH"}],
+        BillingMode="PAY_PER_REQUEST",
+    )
+
+
+@pytest_asyncio.fixture()
+async def session(moto_container: MotoContainer) -> DynamoDBSession:
+    def client_factory() -> DynamoDBClient:
+        return get_session().create_client("dynamodb", **moto_container.get_aws_client_config())
+
+    return DynamoDBSession(client_factory)
+
+
+@pytest.mark.asyncio()
+async def test_commit_session(session: DynamoDBSession, moto_dynamodb_client: DynamoDBClient) -> None:
+    transact_item: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-1111"}},
+        }
+    }
+    session.add(transact_item)
+
+    await session.commit()
+
+    get_item_response = await moto_dynamodb_client.get_item(TableName="TestTable", Key={"Id": {"S": "item-1111"}})
+    assert get_item_response["Item"] == {"Id": {"S": "item-1111"}}
+
+
+@pytest.mark.asyncio()
+async def test_rollback_session(session: DynamoDBSession, moto_dynamodb_client: DynamoDBClient) -> None:
+    transact_item: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-1111"}},
+        }
+    }
+    session.add(transact_item)
+
+    session.rollback()
+    await session.commit()
+
+    get_item_response = await moto_dynamodb_client.get_item(TableName="TestTable", Key={"Id": {"S": "item-1111"}})
+    assert "Item" not in get_item_response
+
+
+@pytest.mark.asyncio()
+async def test_session_cleared_after_commit(session: DynamoDBSession) -> None:
+    transact_item: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-1111"}},
+            "ConditionExpression": "attribute_not_exists(Id)",
+        },
+    }
+    session.add(transact_item)
+
+    await session.commit()
+    await session.commit()  # Should not attempt to commit the item again, otherwise will fail on ConditionExpression
+
+
+@pytest.mark.asyncio()
+async def test_raise_dynamodb_exception(session: DynamoDBSession, moto_dynamodb_client: DynamoDBClient) -> None:
+    transact_item: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-1111"}},
+            "ConditionExpression": "attribute_not_exists(Id)",
+        },
+    }
+    session.add(transact_item)
+    await session.commit()
+
+    session.add(transact_item)
+
+    with pytest.raises(ClientError) as exc_info:
+        await session.commit()
+    assert exc_info.value.response["Error"]["Code"] == "TransactionCanceledException"
+    get_item_response = await moto_dynamodb_client.get_item(TableName="TestTable", Key={"Id": {"S": "item-1111"}})
+    assert "Item" in get_item_response
+
+
+@pytest.mark.asyncio()
+async def test_raise_domain_exception(session: DynamoDBSession, moto_dynamodb_client: DynamoDBClient) -> None:
+    transact_item: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-1111"}},
+            "ConditionExpression": "attribute_not_exists(Id)",
+        },
+    }
+    session.add(transact_item)
+    await session.commit()
+
+    session.add(transact_item, raise_on_condition_check_failure=RuntimeError("Item already exists"))
+
+    with pytest.raises(RuntimeError, match="Item already exists"):
+        await session.commit()
+    get_item_response = await moto_dynamodb_client.get_item(TableName="TestTable", Key={"Id": {"S": "item-1111"}})
+    assert "Item" in get_item_response
+
+
+@pytest.mark.asyncio()
+async def test_dynamodb_exception_raised_for_first_failing_item(session: DynamoDBSession) -> None:
+    session.add({"Put": {"TableName": "TestTable", "Item": {"Id": {"S": "item-2222"}}}})
+    await session.commit()
+
+    passing_item_1: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-1111"}},
+        }
+    }
+    failing_item_2: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-2222"}},
+            "ConditionExpression": "attribute_not_exists(Id)",  # This will fail
+        }
+    }
+    passing_item_3: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-3333"}},
+        }
+    }
+    session.add(passing_item_1)
+    session.add(failing_item_2)
+    session.add(passing_item_3)
+
+    with pytest.raises(ClientError) as exc_info:
+        await session.commit()
+    assert exc_info.value.response["Error"]["Code"] == "TransactionCanceledException"
+    cancellation_codes = [reason["Code"] for reason in exc_info.value.response["CancellationReasons"]]
+    assert cancellation_codes == ["None", "ConditionalCheckFailed", "None"]
+
+
+@pytest.mark.asyncio()
+async def test_domain_error_raised_for_first_failing_item(session: DynamoDBSession) -> None:
+    session.add({"Put": {"TableName": "TestTable", "Item": {"Id": {"S": "item-2222"}}}})
+    await session.commit()
+
+    passing_item_1: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-1111"}},
+        }
+    }
+    failing_item_2: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-2222"}},
+            "ConditionExpression": "attribute_not_exists(Id)",  # This will fail
+        }
+    }
+    passing_item_3: TransactWriteItemTypeDef = {
+        "Put": {
+            "TableName": "TestTable",
+            "Item": {"Id": {"S": "item-3333"}},
+        }
+    }
+    session.add(passing_item_1)
+    session.add(failing_item_2, raise_on_condition_check_failure=RuntimeError("Item already exists"))
+    session.add(passing_item_3)
+
+    with pytest.raises(RuntimeError, match="Item already exists"):
+        await session.commit()
