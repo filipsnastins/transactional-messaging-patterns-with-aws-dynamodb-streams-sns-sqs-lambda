dac870ce0068550cf668015c1d4057c77a69c2b6
Filips Nastins <nastinsfilips@gmail.com>
2023-08-14 18:39:58 +0200

    feat: idempotent consumer handler

---
+++ library-transactional-outbox/tests/test_idempotent_consumer.py
@@ -0,0 +1,32 @@
+import uuid
+
+import pytest
+
+from transactional_outbox.fakes import FakeInboxRepository
+from transactional_outbox.idempotent_consumer import MessageAlreadyProcessedError, ensure_idempotence
+
+
+@pytest.fixture()
+def repo() -> FakeInboxRepository:
+    return FakeInboxRepository([])
+
+
+@pytest.mark.asyncio()
+async def test_new_message_processed(repo: FakeInboxRepository) -> None:
+    message_id_1 = uuid.uuid4()
+    message_id_2 = uuid.uuid4()
+
+    await ensure_idempotence(message_id_1, repo)
+    await ensure_idempotence(message_id_2, repo)
+
+    assert await repo.get(message_id_1)
+    assert await repo.get(message_id_2)
+
+
+@pytest.mark.asyncio()
+async def test_already_processed_message(repo: FakeInboxRepository) -> None:
+    message_id = uuid.uuid4()
+    await ensure_idempotence(message_id, repo)
+
+    with pytest.raises(MessageAlreadyProcessedError, match=str(message_id)):
+        await ensure_idempotence(message_id, repo)
