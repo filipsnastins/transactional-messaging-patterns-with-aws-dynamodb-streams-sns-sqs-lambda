eb48e49876697678d72fa60bc560785bd3a65a42
Filips Nastins <nastinsfilips@gmail.com>
2023-08-14 18:39:58 +0200

    feat: idempotent consumer handler

--- library-transactional-outbox/src/transactional_outbox/idempotent_consumer.py
+++ library-transactional-outbox/src/transactional_outbox/idempotent_consumer.py
@@ -3,6 +3,10 @@ import uuid
 from dataclasses import dataclass
 from typing import Protocol
 
+import structlog
+
+logger: structlog.stdlib.BoundLogger = structlog.get_logger()
+
 
 class MessageAlreadyProcessedError(Exception):
     pass
@@ -20,3 +24,11 @@ class InboxRepository(Protocol):
 
     async def get(self, message_id: uuid.UUID) -> ProcessedMessage | None:
         ...
+
+
+async def ensure_idempotence(message_id: uuid.UUID, repository: InboxRepository) -> None:
+    processed_message = await repository.get(message_id=message_id)
+    if processed_message is not None:
+        logger.info("idempotent_consumer__message_already_processed", message_id=message_id)
+        raise MessageAlreadyProcessedError(message_id)
+    await repository.save(message_id=message_id)
