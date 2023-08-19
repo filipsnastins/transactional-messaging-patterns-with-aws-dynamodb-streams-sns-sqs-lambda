a8cbb6ddf0b33d9767609c22036dd5598f4acbc9
Filips Nastins <nastinsfilips@gmail.com>
2023-08-19 09:58:51 +0200

    chore: update outbox

--- service-customers/src/entrypoints/tomodachi_app.py
+++ service-customers/src/entrypoints/tomodachi_app.py
@@ -23,27 +23,28 @@ class TomodachiService(tomodachi.Service):
     name = "service-customers"
 
     def __init__(self) -> None:
-        settings = get_settings()
+        self.settings = get_settings()
         self.options = tomodachi.Options(
             aws_endpoint_urls=tomodachi.Options.AWSEndpointURLs(
-                sns=settings.aws_endpoint_url,
-                sqs=settings.aws_endpoint_url,
+                sns=self.settings.aws_endpoint_url,
+                sqs=self.settings.aws_endpoint_url,
             ),
             aws_sns_sqs=tomodachi.Options.AWSSNSSQS(
-                region_name=settings.aws_region,
-                aws_access_key_id=settings.aws_access_key_id,
-                aws_secret_access_key=settings.aws_secret_access_key,
-                topic_prefix=settings.aws_sns_topic_prefix,
-                queue_name_prefix=settings.aws_sqs_queue_name_prefix,
+                region_name=self.settings.aws_region,
+                aws_access_key_id=self.settings.aws_access_key_id,
+                aws_secret_access_key=self.settings.aws_secret_access_key,
+                topic_prefix=self.settings.aws_sns_topic_prefix,
+                queue_name_prefix=self.settings.aws_sqs_queue_name_prefix,
             ),
         )
 
     async def _start_service(self) -> None:
-        await sns.create_topics()
-        await dynamodb.create_customers_table()
-        await inbox.create_inbox_table()
-        await outbox.create_outbox_table()
-        await outbox.create_dynamodb_streams_outbox()
+        if self.settings.environment in ["development", "autotest"]:
+            await sns.create_topics()
+            await dynamodb.create_customers_table()
+            await inbox.create_inbox_table()
+            await outbox.create_outbox_table()
+            await outbox.create_dynamodb_streams_outbox()
 
     @tomodachi.http("GET", r"/customers/health/?", ignore_logging=[200])
     async def healthcheck(self, request: web.Request) -> web.Response:
@@ -70,7 +71,7 @@ class TomodachiService(tomodachi.Service):
             response = await views.get_customer(uow, customer_id=uuid.UUID(customer_id))
             return web.json_response(response.to_dict(), status=STATUS_CODES[response.type])
 
-    @tomodachi.aws_sns_sqs("order--created", queue="customer--order-created", message_envelope=JsonBase)
+    @tomodachi.aws_sns_sqs("order--created", queue_name="customer--order-created", message_envelope=JsonBase)
     async def order_created_handler(self, data: dict) -> None:
         async with DynamoDBUnitOfWork(message_id=data["event_id"]) as uow:
             event = ReserveCreditCommand(
@@ -81,7 +82,7 @@ class TomodachiService(tomodachi.Service):
             )
             await use_cases.reserve_credit(uow, event)
 
-    @tomodachi.aws_sns_sqs("order--cancelled", queue="customer--order-cancelled", message_envelope=JsonBase)
+    @tomodachi.aws_sns_sqs("order--cancelled", queue_name="customer--order-cancelled", message_envelope=JsonBase)
     async def order_cancelled_handler(self, data: dict) -> None:
         async with DynamoDBUnitOfWork(message_id=data["event_id"]) as uow:
             event = ReleaseCreditCommand(
