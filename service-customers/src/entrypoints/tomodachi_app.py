40f9564c7cfbda7bd2f288a5f093338cc7590adb
Filips Nastins <nastinsfilips@gmail.com>
2023-08-19 14:59:23 +0200

    feat: correlation id middleware and dlq

--- service-customers/src/entrypoints/tomodachi_app.py
+++ service-customers/src/entrypoints/tomodachi_app.py
@@ -8,7 +8,14 @@ from tomodachi.envelope.json_base import JsonBase
 from adapters import dynamodb, inbox, outbox, sns
 from adapters.settings import get_settings
 from customers.commands import CreateCustomerCommand, ReleaseCreditCommand, ReserveCreditCommand
+from entrypoints.middleware import (
+    http_correlation_id_middleware,
+    message_correlation_id_middleware,
+    message_retry_middleware,
+    structlog_logger_middleware,
+)
 from service_layer import use_cases, views
+from service_layer.logger import configure_structlog
 from service_layer.response import ResponseTypes
 from service_layer.unit_of_work import DynamoDBUnitOfWork
 
@@ -22,7 +29,18 @@ STATUS_CODES: dict[ResponseTypes, int] = {
 class TomodachiService(tomodachi.Service):
     name = "service-customers"
 
+    http_middleware: list = [
+        http_correlation_id_middleware,
+        structlog_logger_middleware,
+    ]
+    message_middleware: list = [
+        message_retry_middleware,
+        message_correlation_id_middleware,
+        structlog_logger_middleware,
+    ]
+
     def __init__(self) -> None:
+        configure_structlog()
         self.settings = get_settings()
         self.options = tomodachi.Options(
             aws_endpoint_urls=tomodachi.Options.AWSEndpointURLs(
@@ -47,46 +65,77 @@ class TomodachiService(tomodachi.Service):
             await outbox.create_dynamodb_streams_outbox()
 
     @tomodachi.http("GET", r"/customers/health/?", ignore_logging=[200])
-    async def healthcheck(self, request: web.Request) -> web.Response:
-        return web.json_response({"status": "ok"}, status=200)
+    async def healthcheck(self, request: web.Request, correlation_id: uuid.UUID) -> web.Response:
+        return web.json_response(
+            {"status": "ok"},
+            status=200,
+            headers={"X-Correlation-Id": str(correlation_id)},
+        )
 
     @tomodachi.http_error(status_code=500)
-    async def error_500(self, request: web.Request) -> web.Response:
-        return web.json_response({"error": ResponseTypes.SYSTEM_ERROR.value}, status=500)
+    async def error_500(self, request: web.Request, correlation_id: uuid.UUID) -> web.Response:
+        return web.json_response(
+            {"error": ResponseTypes.SYSTEM_ERROR.value},
+            status=500,
+            headers={"X-Correlation-Id": str(correlation_id)},
+        )
 
     @tomodachi.http("POST", r"/customers")
-    async def create_customer_handler(self, request: web.Request) -> web.Response:
+    async def create_customer_handler(self, request: web.Request, correlation_id: uuid.UUID) -> web.Response:
         async with DynamoDBUnitOfWork() as uow:
             data = await request.json()
             cmd = CreateCustomerCommand(
+                correlation_id=correlation_id,
                 name=str(data["name"]),
                 credit_limit=Money.from_sub_units(int(data["credit_limit"])).as_decimal(),
             )
             response = await use_cases.create_customer(uow, cmd)
-            return web.json_response(response.to_dict(), status=STATUS_CODES[response.type])
+            return web.json_response(
+                response.to_dict(),
+                status=STATUS_CODES[response.type],
+                headers={"X-Correlation-Id": str(correlation_id)},
+            )
 
     @tomodachi.http("GET", r"/customer/(?P<customer_id>[^/]+?)/?")
-    async def get_customer_handler(self, request: web.Request, customer_id: str) -> web.Response:
+    async def get_customer_handler(
+        self, request: web.Request, customer_id: str, correlation_id: uuid.UUID
+    ) -> web.Response:
         async with DynamoDBUnitOfWork() as uow:
             response = await views.get_customer(uow, customer_id=uuid.UUID(customer_id))
-            return web.json_response(response.to_dict(), status=STATUS_CODES[response.type])
+            return web.json_response(
+                response.to_dict(),
+                status=STATUS_CODES[response.type],
+                headers={"X-Correlation-Id": str(correlation_id)},
+            )
 
-    @tomodachi.aws_sns_sqs("order--created", queue_name="customer--order-created", message_envelope=JsonBase)
-    async def order_created_handler(self, data: dict) -> None:
-        async with DynamoDBUnitOfWork(message_id=data["event_id"]) as uow:
+    @tomodachi.aws_sns_sqs(
+        "order--created",
+        queue_name="customer--order-created",
+        dead_letter_queue_name="customer--order-created--dlq",
+        max_receive_count=3,
+        message_envelope=JsonBase,
+    )
+    async def order_created_handler(self, data: dict, correlation_id: uuid.UUID) -> None:
+        async with DynamoDBUnitOfWork(message_id=uuid.UUID(data["event_id"])) as uow:
             event = ReserveCreditCommand(
-                correlation_id=uuid.UUID(data["correlation_id"]),
+                correlation_id=correlation_id,
                 order_id=uuid.UUID(data["order_id"]),
                 customer_id=uuid.UUID(data["customer_id"]),
                 order_total=Money.from_sub_units(int(data["order_total"])).as_decimal(),
             )
             await use_cases.reserve_credit(uow, event)
 
-    @tomodachi.aws_sns_sqs("order--cancelled", queue_name="customer--order-cancelled", message_envelope=JsonBase)
-    async def order_cancelled_handler(self, data: dict) -> None:
-        async with DynamoDBUnitOfWork(message_id=data["event_id"]) as uow:
+    @tomodachi.aws_sns_sqs(
+        "order--cancelled",
+        queue_name="customer--order-cancelled",
+        dead_letter_queue_name="customer--order-cancelled--dlq",
+        max_receive_count=3,
+        message_envelope=JsonBase,
+    )
+    async def order_cancelled_handler(self, data: dict, correlation_id: uuid.UUID) -> None:
+        async with DynamoDBUnitOfWork(message_id=uuid.UUID(data["event_id"])) as uow:
             event = ReleaseCreditCommand(
-                correlation_id=uuid.UUID(data["correlation_id"]),
+                correlation_id=correlation_id,
                 order_id=uuid.UUID(data["order_id"]),
                 customer_id=uuid.UUID(data["customer_id"]),
             )
