86f2521fafe709e4f6805ba42cf9dd2f02dd80a3
Filips Nastins <nastinsfilips@gmail.com>
2023-08-20 12:06:28 +0200

    test: test uuid cast to str in logger

--- library-tomodachi-bootstrap/tests/test_logger.py
+++ library-tomodachi-bootstrap/tests/test_logger.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import uuid
 
 import pytest
 import structlog
@@ -14,7 +15,6 @@ def test_default_json_renderer(caplog: pytest.LogCaptureFixture) -> None:
 
     logger.info("test_log")
 
-    assert caplog.record_tuples
     assert len(caplog.record_tuples) == 1
     record = caplog.record_tuples[0]
     assert record[0] == "tests.test_logger"
@@ -26,3 +26,16 @@ def test_default_json_renderer(caplog: pytest.LogCaptureFixture) -> None:
         "level": "info",
         "timestamp": payload["timestamp"],
     }
+
+
+def test_uuid_casted_to_string(caplog: pytest.LogCaptureFixture) -> None:
+    configure_structlog()
+    structlog.configure(cache_logger_on_first_use=False)
+    logger: structlog.stdlib.BoundLogger = structlog.get_logger()
+    correlation_id = uuid.UUID("430d1506-5e9c-467d-a303-751d2ddee79d")
+
+    logger.info("test_log", correlation_id=correlation_id)
+
+    assert len(caplog.record_tuples) == 1
+    payload = json.loads(caplog.record_tuples[0][2])
+    assert payload["correlation_id"] == "430d1506-5e9c-467d-a303-751d2ddee79d"
