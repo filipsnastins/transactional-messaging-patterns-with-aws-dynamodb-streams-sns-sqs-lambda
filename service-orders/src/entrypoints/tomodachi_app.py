86a309224bac8f082fd777c8cc13f358a44fc6f8
Filips Nastins <nastinsfilips@gmail.com>
2023-08-20 12:33:10 +0200

    feat: use tomodachi bootstrap for base service functionality

--- service-orders/src/entrypoints/tomodachi_app.py
+++ service-orders/src/entrypoints/tomodachi_app.py
@@ -4,18 +4,11 @@ import tomodachi
 from aiohttp import web
 from stockholm import Money
 from tomodachi.envelope.json_base import JsonBase
+from tomodachi_bootstrap import TomodachiServiceBase
 
 from adapters import dynamodb, inbox, outbox, sns
-from adapters.settings import get_settings
-from entrypoints.middleware import (
-    http_correlation_id_middleware,
-    message_correlation_id_middleware,
-    message_retry_middleware,
-    structlog_middleware,
-)
 from orders.commands import ApproveOrderCommand, CancelOrderCommand, CreateOrderCommand, RejectOrderCommand
 from service_layer import use_cases, views
-from service_layer.logger import configure_structlog
 from service_layer.response import ResponseTypes
 from service_layer.unit_of_work import DynamoDBUnitOfWork
 
@@ -28,38 +21,8 @@ STATUS_CODES: dict[ResponseTypes, int] = {
 }
 
 
-class TomodachiService(tomodachi.Service):
+class TomodachiService(TomodachiServiceBase):
     name = "service-orders"
-
-    http_middleware: list = [
-        http_correlation_id_middleware,
-        structlog_middleware,
-    ]
-    message_middleware: list = [
-        message_retry_middleware,
-        message_correlation_id_middleware,
-        structlog_middleware,
-    ]
-
-    def __init__(self) -> None:
-        self.settings = get_settings()
-
-        self.is_dev_env = self.settings.environment in ["development", "autotest"]
-        configure_structlog(renderer="dev" if self.is_dev_env else "json")
-
-        self.options = tomodachi.Options(
-            aws_endpoint_urls=tomodachi.Options.AWSEndpointURLs(
-                sns=self.settings.aws_endpoint_url,
-                sqs=self.settings.aws_endpoint_url,
-            ),
-            aws_sns_sqs=tomodachi.Options.AWSSNSSQS(
-                region_name=self.settings.aws_region,
-                aws_access_key_id=self.settings.aws_access_key_id,
-                aws_secret_access_key=self.settings.aws_secret_access_key,
-                topic_prefix=self.settings.aws_sns_topic_prefix,
-                queue_name_prefix=self.settings.aws_sqs_queue_name_prefix,
-            ),
-        )
 
     async def _start_service(self) -> None:
         if self.is_dev_env:
@@ -71,19 +34,11 @@ class TomodachiService(tomodachi.Service):
 
     @tomodachi.http("GET", r"/orders/health/?", ignore_logging=[200])
     async def healthcheck(self, request: web.Request, correlation_id: uuid.UUID) -> web.Response:
-        return web.json_response(
-            {"status": "ok"},
-            status=200,
-            headers={"X-Correlation-Id": str(correlation_id)},
-        )
+        return web.json_response({"status": "ok"}, status=200)
 
     @tomodachi.http_error(status_code=500)
     async def error_500(self, request: web.Request, correlation_id: uuid.UUID) -> web.Response:
-        return web.json_response(
-            {"error": ResponseTypes.SYSTEM_ERROR.value},
-            status=500,
-            headers={"X-Correlation-Id": str(correlation_id)},
-        )
+        return web.json_response({"error": ResponseTypes.SYSTEM_ERROR.value}, status=500)
 
     @tomodachi.http("POST", r"/orders")
     async def create_order_handler(self, request: web.Request, correlation_id: uuid.UUID) -> web.Response:
@@ -95,21 +50,13 @@ class TomodachiService(tomodachi.Service):
                 order_total=Money.from_sub_units(int(data["order_total"])).as_decimal(),
             )
             response = await use_cases.create_order(uow, cmd)
-            return web.json_response(
-                response.to_dict(),
-                status=STATUS_CODES[response.type],
-                headers={"X-Correlation-Id": str(correlation_id)},
-            )
+            return web.json_response(response.to_dict(), status=STATUS_CODES[response.type])
 
     @tomodachi.http("GET", r"/order/(?P<order_id>[^/]+?)/?")
     async def get_order_handler(self, request: web.Request, order_id: str, correlation_id: uuid.UUID) -> web.Response:
         async with DynamoDBUnitOfWork() as uow:
             response = await views.get_order(uow, order_id=uuid.UUID(order_id))
-            return web.json_response(
-                response.to_dict(),
-                status=STATUS_CODES[response.type],
-                headers={"X-Correlation-Id": str(correlation_id)},
-            )
+            return web.json_response(response.to_dict(), status=STATUS_CODES[response.type])
 
     @tomodachi.http("POST", r"/order/(?P<order_id>[^/]+?)/cancel?")
     async def cancel_order_handler(
@@ -118,11 +65,7 @@ class TomodachiService(tomodachi.Service):
         async with DynamoDBUnitOfWork() as uow:
             cmd = CancelOrderCommand(correlation_id=correlation_id, order_id=uuid.UUID(order_id))
             response = await use_cases.cancel_order(uow, cmd)
-            return web.json_response(
-                response.to_dict(),
-                status=STATUS_CODES[response.type],
-                headers={"X-Correlation-Id": str(correlation_id)},
-            )
+            return web.json_response(response.to_dict(), status=STATUS_CODES[response.type])
 
     @tomodachi.aws_sns_sqs(
         "customer--credit-reserved",
